74a517e5ceb1e3eb787b272aa85b54a453ca485e
Kei <kei.patheng@gmail.com>
2024-11-13 18:22:18 +0900

    create env.production file

--- backend/dist/server.mjs
+++ backend/dist/server.mjs
@@ -3,7 +3,7 @@ import completionRouter from "./routes/completion.mjs";
 import mypageRouter from "./routes/mypage.mjs";
 import "./helpers/db.mjs";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/completion", completionRouter);
 app.use("/api/mypage", mypageRouter);
