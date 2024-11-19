600458335f9c8979b11c5a224005f6c5292f8eaf
Kei <kei.patheng@gmail.com>
2024-11-19 14:38:11 +0900

    Remove dist and node_modules from repository

--- backend/dist/helpers/prompt.js
+++
@@ -1,8 +0,0 @@
-const requestBody = [
-    { role: "system", content: "You are a helpful assistant." },
-    {
-        role: "user",
-        content: "Write a haiku about recursion in programming.",
-    },
-];
-export default requestBody;
--- backend/dist/models/result.mjs
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import { Schema } from "mongoose";
-const resultSchema = new Schema({
-    userId: { type: String, required: true },
-    recommendedFoods: { type: String, required: true },
-    missingNutrients: { type: String, required: true },
-}, { timestamps: true });
-const Result = mongoose.model("Result", resultSchema);
-export default Result;
